e018771a4388378b0130ad8fae6b143005d0958b
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-21 14:35:55 +0200

    Перероблено функцію createItem

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,9 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const galleryEl = document.querySelector(".gallery");
 const galleryNodes = [];
 
-// const createItem = (...item) => {
-//   console.log(item);
-// //   const galleryItem = document.createElement("div");
-// //   galleryItem.classList.add("gallery__item");
-
-// //   const galleryLink = document.createElement("a");
-// //   galleryLink.classList.add("gallery__link");
-// //   galleryLink.href = original;
-
-// //   const galleryImage = document.createElement("img");
-// //   galleryImage.classList.add("gallery__image");
-// //   galleryImage.src = preview;
-// //   galleryImage.dataset.source = original;
-// //   galleryImage.alt = description;
-
-// //   galleryLink.append(galleryImage);
-// //   galleryItem.append(galleryLink);
-
-// //   galleryNodes.push(galleryItem);
-// };
-
-function createItem({ original, description, preview }) {
+const createItem = ({ original, description, preview }) => {
   const galleryItem = document.createElement("div");
   galleryItem.classList.add("gallery__item");
 
@@ -43,18 +21,68 @@ function createItem({ original, description, preview }) {
   galleryItem.append(galleryLink);
 
   galleryNodes.push(galleryItem);
-}
-
-function openImage(event) {
-    event.preventDefault();
-}
+};
 
 galleryItems.forEach((item) => createItem(item));
 galleryEl.append(...galleryNodes);
 
-galleryEl.addEventListener("click", openImage);
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
 
-//console.log(galleryItems);
-//console.log(galleryEl);
-//console.log(galleryEl);
-//{ original, description, preview }
+// const galleryEl = document.querySelector(".gallery");
+// const galleryNodes = [];
+
+// // const createItem = (...item) => {
+// //   console.log(item);
+// // //   const galleryItem = document.createElement("div");
+// // //   galleryItem.classList.add("gallery__item");
+
+// // //   const galleryLink = document.createElement("a");
+// // //   galleryLink.classList.add("gallery__link");
+// // //   galleryLink.href = original;
+
+// // //   const galleryImage = document.createElement("img");
+// // //   galleryImage.classList.add("gallery__image");
+// // //   galleryImage.src = preview;
+// // //   galleryImage.dataset.source = original;
+// // //   galleryImage.alt = description;
+
+// // //   galleryLink.append(galleryImage);
+// // //   galleryItem.append(galleryLink);
+
+// // //   galleryNodes.push(galleryItem);
+// // };
+
+// function createItem({ original, description, preview }) {
+//   const galleryItem = document.createElement("div");
+//   galleryItem.classList.add("gallery__item");
+
+//   const galleryLink = document.createElement("a");
+//   galleryLink.classList.add("gallery__link");
+//   galleryLink.href = original;
+
+//   const galleryImage = document.createElement("img");
+//   galleryImage.classList.add("gallery__image");
+//   galleryImage.src = preview;
+//   galleryImage.dataset.source = original;
+//   galleryImage.alt = description;
+
+//   galleryLink.append(galleryImage);
+//   galleryItem.append(galleryLink);
+
+//   galleryNodes.push(galleryItem);
+// }
+
+// function openImage(event) {
+//     event.preventDefault();
+// }
+
+// galleryItems.forEach((item) => createItem(item));
+// galleryEl.append(...galleryNodes);
+
+// galleryEl.addEventListener("click", openImage);
+
+// //console.log(galleryItems);
+// //console.log(galleryEl);
+// //console.log(galleryEl);
+// //{ original, description, preview }
