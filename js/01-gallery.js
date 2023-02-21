a1887af32e7d1a7aeb39638a4179cb325423580c
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-21 14:25:21 +0200

    Додано розмітку для галереї та скасовано діїї за замовчуванням

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryNodes = [];
+
+// const createItem = (...item) => {
+//   console.log(item);
+// //   const galleryItem = document.createElement("div");
+// //   galleryItem.classList.add("gallery__item");
+
+// //   const galleryLink = document.createElement("a");
+// //   galleryLink.classList.add("gallery__link");
+// //   galleryLink.href = original;
+
+// //   const galleryImage = document.createElement("img");
+// //   galleryImage.classList.add("gallery__image");
+// //   galleryImage.src = preview;
+// //   galleryImage.dataset.source = original;
+// //   galleryImage.alt = description;
+
+// //   galleryLink.append(galleryImage);
+// //   galleryItem.append(galleryLink);
+
+// //   galleryNodes.push(galleryItem);
+// };
+
+function createItem({ original, description, preview }) {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = preview;
+  galleryImage.dataset.source = original;
+  galleryImage.alt = description;
+
+  galleryLink.append(galleryImage);
+  galleryItem.append(galleryLink);
+
+  galleryNodes.push(galleryItem);
+}
+
+function openImage(event) {
+    event.preventDefault();
+}
+
+galleryItems.forEach((item) => createItem(item));
+galleryEl.append(...galleryNodes);
+
+galleryEl.addEventListener("click", openImage);
+
+//console.log(galleryItems);
+//console.log(galleryEl);
+//console.log(galleryEl);
+//{ original, description, preview }
