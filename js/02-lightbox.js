6348479a60898e35142b177b9133b221a9975f55
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-22 21:33:39 +0200

    Створення і рендер розмітки для завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,17 +5,16 @@ const galleryEl = document.querySelector(".gallery");
 const galleryNodes = [];
 
 const createItem = ({ original, description, preview }) => {
-  const galleryItem = document.createElement("div");
+  const galleryItem = document.createElement("li");
   galleryItem.classList.add("gallery__item");
 
   const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__link");
+  galleryLink.classList.add("gallery__item");
   galleryLink.href = original;
 
   const galleryImage = document.createElement("img");
   galleryImage.classList.add("gallery__image");
   galleryImage.src = preview;
-  galleryImage.dataset.source = original;
   galleryImage.alt = description;
 
   galleryLink.append(galleryImage);
@@ -24,29 +23,7 @@ const createItem = ({ original, description, preview }) => {
   galleryNodes.push(galleryItem);
 };
 
-const openImage = (event) => {
-  event.preventDefault();
-
-  const originalImgUrl = event.target.dataset.source;
-
-  const instance = basicLightbox.create(
-    `<img width="auto" height="auto" src="${originalImgUrl}">`,
-    {
-      onShow: () => window.addEventListener("keydown", onCloseWithEscape),
-      onClose: () => window.removeEventListener("keydown", onCloseWithEscape),
-    }
-  );
-
-  const onCloseWithEscape = (event) => {
-    if (event.code === "Escape") {
-      instance.close();
-    }
-  };
-
-  instance.show();
-};
-
 galleryItems.forEach((item) => createItem(item));
 galleryEl.append(...galleryNodes);
 
-galleryEl.addEventListener("click", openImage);
+//galleryEl.addEventListener("click", openImage);
