448d210b009e8f7332edc6344e6507a77827fdb4
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-02-23 09:11:49 +0200

    Виконано рефактринг коду для завдання 1
    Додано коментарі та JSdocs

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,112 +1,65 @@
 import { galleryItems } from "./gallery-items.js";
 
 const galleryEl = document.querySelector(".gallery");
-const galleryNodes = [];
+const galleryNodes = []; // Масив для зберігання окремих вузлів "div"
 
+/**
+ * Функція створює розмітку елементів галереї
+ * @param {{ original, description, preview }} Деструктуризація об'єкту параметрів galleryItems
+ * @returns {void}
+ */
 const createItem = ({ original, description, preview }) => {
-  const galleryItem = document.createElement("div");
-  galleryItem.classList.add("gallery__item");
+  const galleryItem = document.createElement("div");         // Створення окремого елемента "div"
+  galleryItem.classList.add("gallery__item");                // Додавання класу до створеного "div"
 
-  const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__link");
-  galleryLink.href = original;
+  const galleryLink = document.createElement("a");           // Створення окремого елемента "a"
+  galleryLink.classList.add("gallery__link");                // Додавання класу до створеного "a"
+  galleryLink.href = original;                               // Вказує посилання на оригінальне зображення в "href"
 
-  const galleryImage = document.createElement("img");
-  galleryImage.classList.add("gallery__image");
-  galleryImage.src = preview;
-  galleryImage.dataset.source = original;
-  galleryImage.alt = description;
+  const galleryImage = document.createElement("img");        // Створення окремого елемента "img"
+  galleryImage.classList.add("gallery__image");              // Додавання класу до створеного "img"
+  galleryImage.src = preview;                                // Вказує посилання на зображення для preview в "src"
+  galleryImage.dataset.source = original;                    // Вказує посилання на оригінальне зображення в "data-атрибуті"
+  galleryImage.alt = description;                            // Вказує текст для атрибуту "alt"
 
-  galleryLink.append(galleryImage);
-  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);                          // Додає елемент "img" в елемент "a"
+  galleryItem.append(galleryLink);                           // Додає елемент "a" в елемент "div"
 
-  galleryNodes.push(galleryItem);
+  galleryNodes.push(galleryItem);                            // Додає готовий елемент "div" в масив
 };
 
+/**
+ * Функція відкриває модальне вікно і показує оригінальне зображення
+ * @param {event} Об'єкт події "click"
+ * @returns {void}
+ */
 const openImage = (event) => {
-  event.preventDefault();
+  event.preventDefault(); // Скасовує поведінку за замовчуванням
 
-  const originalImgUrl = event.target.dataset.source;
+  if (event.target.nodeName !== "IMG") return; // Припиняє виконання функції якщо клік був не на елементі <img>
 
-  const instance = basicLightbox.create(
-    `<img width="auto" height="auto" src="${originalImgUrl}">`,
-    {
-      onShow: () => window.addEventListener("keydown", onCloseWithEscape),
-      onClose: () => window.removeEventListener("keydown", onCloseWithEscape),
-    }
-  );
+  const originalImgUrl = event.target.dataset.source; // Отримує посилання на оригінальне зображення за "data-атрибуту"
 
-  const onCloseWithEscape = (event) => {
+  const instance = basicLightbox.create(`<img src="${originalImgUrl}">`, {
+    onShow: () => window.addEventListener("keydown", onCloseByEsc),     // Реєструє обробник події "keydown"
+    onClose: () => window.removeEventListener("keydown", onCloseByEsc), // Видаляє відповідний обробник події "keydown"
+  });
+
+  /**
+   * Фунція перевіряє чи була натиснута клавіша "Esc". Якщо так, то викликає функцію для видалення модального вікна
+   * @param {event} Об'єкт події "keydown"
+   * @returns {void}
+   */
+  const onCloseByEsc = (event) => {
     if (event.code === "Escape") {
-      instance.close();
+      instance.close(); // викликає функцію для видалення модального вікна
     }
   };
 
-  instance.show();
+  instance.show(); // Рендерить модальне вікно з оригінальним зображенням
 };
 
 galleryItems.forEach((item) => createItem(item));
-galleryEl.append(...galleryNodes);
+galleryEl.append(...galleryNodes); // Додає готові вузли розмітки в контейнер галереї
 
 galleryEl.addEventListener("click", openImage);
-
-// import { galleryItems } from "./gallery-items.js";
-// // Change code below this line
-
-// const galleryEl = document.querySelector(".gallery");
-// const galleryNodes = [];
-
-// // const createItem = (...item) => {
-// //   console.log(item);
-// // //   const galleryItem = document.createElement("div");
-// // //   galleryItem.classList.add("gallery__item");
-
-// // //   const galleryLink = document.createElement("a");
-// // //   galleryLink.classList.add("gallery__link");
-// // //   galleryLink.href = original;
-
-// // //   const galleryImage = document.createElement("img");
-// // //   galleryImage.classList.add("gallery__image");
-// // //   galleryImage.src = preview;
-// // //   galleryImage.dataset.source = original;
-// // //   galleryImage.alt = description;
-
-// // //   galleryLink.append(galleryImage);
-// // //   galleryItem.append(galleryLink);
-
-// // //   galleryNodes.push(galleryItem);
-// // };
-
-// function createItem({ original, description, preview }) {
-//   const galleryItem = document.createElement("div");
-//   galleryItem.classList.add("gallery__item");
-
-//   const galleryLink = document.createElement("a");
-//   galleryLink.classList.add("gallery__link");
-//   galleryLink.href = original;
-
-//   const galleryImage = document.createElement("img");
-//   galleryImage.classList.add("gallery__image");
-//   galleryImage.src = preview;
-//   galleryImage.dataset.source = original;
-//   galleryImage.alt = description;
-
-//   galleryLink.append(galleryImage);
-//   galleryItem.append(galleryLink);
-
-//   galleryNodes.push(galleryItem);
-// }
-
-// function openImage(event) {
-//     event.preventDefault();
-// }
-
-// galleryItems.forEach((item) => createItem(item));
-// galleryEl.append(...galleryNodes);
-
-// galleryEl.addEventListener("click", openImage);
-
-// //console.log(galleryItems);
-// //console.log(galleryEl);
-// //console.log(galleryEl);
-// //{ original, description, preview }
